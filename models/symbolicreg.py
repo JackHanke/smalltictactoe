4dcfbc324fce3a8fc127b4f8416434c36c2c06c6
JackHanke <jack.hanke98@gmail.com>
2026-05-18 22:34:59 -0400

    symreg

--- models/symbolicreg.py
+++ models/symbolicreg.py
@@ -3,10 +3,13 @@ import pandas as pd
 from pysr import PySRRegressor
 
 model = PySRRegressor(
-    maxsize=100,
-    niterations=100,  # < Increase me for better results
+    maxsize=300,
+    niterations=200,  # < Increase me for better results
     binary_operators=["+", "*"],
-
+    unary_operators=[
+        "cos",
+        "sin",
+    ],
     # ^ Define operator for SymPy as well
     elementwise_loss="loss(prediction, target) = (prediction - target)^2",
     # ^ Custom loss function (julia syntax)
@@ -15,13 +18,20 @@ model = PySRRegressor(
 # data = np.genfromtxt(f'data/datasets/ttt_best_bin.csv', delimiter=',', dtype=np.int32, names=True)
 # print(data.shape)
 
-df = pd.read_csv('data/datasets/ttt_best_bin.csv')
-data = df.to_numpy()
+# df = pd.read_csv('data/datasets/ttt_best_bin.csv')
+# data = df.to_numpy()
 
-X = data[:, :17]
-y = data[:, 17:19]
+data = np.loadtxt('data/fixed_arr.txt')
+
+X = data[:, :9]
+y = data[:, 9:]
 
 print(f'X.shape: {X.shape} y.shape: {y.shape}')
 
 model.fit(X,y)
 print(model)
+
+y_pred = model.predict(X)
+
+acc = np.sum(y[:, 0] == np.round(y_pred))/X.shape[0]
+print(f'Model accuracy: {acc*100:.4f}%')
